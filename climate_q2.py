c7333643998a83d9088a0ee825dd60e468054695
kwunmanchau <kwunman.chau@griffithuni.edu.au>
2023-09-14 11:43:33 +1000

    Added code to load data from CSV and create plots

---
+++ climate_q2.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('climate.csv')
+
+years = df['Year']
+co2 = df['CO2']
+temp = df['Temperature']
+
+plt.subplot(2, 1, 1)
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
+
+plt.subplot(2, 1, 2)
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+
+plt.show()
